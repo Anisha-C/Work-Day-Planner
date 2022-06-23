60ade04c284d043c14149c497ba2f716821bbebe
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-06-23 13:48:46 -0400

    Time blocks are created with save feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,41 @@ function currentTime() {
 }
 
 setInterval(currentTime, 1000)
+
+function displayTimeBlocks() {
+    var timeBlocks = "";
+    for (let i = 8; i <= 18; i++) {
+        if (currentHour < i) {
+            var colorCode = "future"
+        }
+        else if (currentHour == i) {
+            var colorCode = "present"
+        }
+        else {
+            var colorCode = "past"
+        }
+        var savedNote = localStorage.getItem(`${i.toString().padStart(2, "0")}:00`) || ""
+        timeBlocks += `<div class="time-block row ${colorCode} hour"  id="${i}"> 
+        <p class = "time col-2">${i.toString().padStart(2, "0")}:00</p>
+        <input type = "text-area" class = col-8 value = ${savedNote}>
+        <button class = "col-2 saveBtn">💾</button>
+        </div>`
+    }
+
+    $("#timeblocks").html(timeBlocks)
+    var saveBtns = document.getElementsByClassName("saveBtn")
+    console.log(saveBtns)
+    for (let i = 0; i < saveBtns.length; i++) {
+        saveBtns[i].addEventListener("click", getNoteData)
+    }
+}
+function getNoteData(event) {
+    var note = $(this).siblings(".col-8").val()
+    var key = $(this).siblings(".time").text()
+    save(key, note)
+}
+function save(key, note) {
+    localStorage.setItem(key, note)
+}
+
+displayTimeBlocks()
